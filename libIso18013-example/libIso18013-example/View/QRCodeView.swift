164d640918fc1472237f3bf870b7d300f8a1972d
Martina D'urso <martina.durso@digitouch.it>
2024-10-17 09:59:12 +0200

    refactor: update app example

--- libIso18013-example/libIso18013-example/View/QRCodeView.swift
+++ libIso18013-example/libIso18013-example/View/QRCodeView.swift
@@ -6,18 +6,38 @@
 //
 
 import SwiftUI
+import libIso18013
 
 struct QRCodeView: View {
+    
+    private var viewModel: QRCodeViewModel = QRCodeViewModel()
+    @State var qrCode: String = ""
+    
     var body: some View {
         VStack {
             Spacer()
-            QRCode()
-                .generateQRCode(from: "example")
+            QRCode
+                .getQrCodeImage(qrCode: qrCode, inputCorrectionLevel: .m)
                 .resizable()
                 .frame(width: 200, height: 200)
             Spacer()
         }
+        .onAppear() {
+            LibIso18013Proximity.shared.setListner(viewModel)
+            do {
+                qrCode = try LibIso18013Proximity.shared.getQrCodePayload()
+            } catch {
+                qrCode = "Error: \(error)"
+            }
+        }
     }
+}
+
+class QRCodeViewModel: QrEngagementListener {
+    func onConnecting() {
+        
+    }
+    
 }
 
 struct QRCodeView_Previews: PreviewProvider {
