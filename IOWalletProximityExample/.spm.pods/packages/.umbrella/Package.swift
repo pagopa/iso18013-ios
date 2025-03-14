e6cfe088d39431364749c7a4f89fda1479dfc9e2
acapadev <antonio.caparello@digitouch.it>
2025-03-14 16:03:26 +0100

    add .spm.pods to .gitignore

--- IOWalletProximityExample/.spm.pods/packages/.umbrella/Package.swift
+++
@@ -1,9 +0,0 @@
-// swift-tools-version:5.7
-import PackageDescription
-
-let package = Package(
-  name: "_umbrella_",
-  dependencies: [
-    .package(path: "/Users/username/repos/digitouch/pagopa1/iso18013-ios")
-  ]
-)
